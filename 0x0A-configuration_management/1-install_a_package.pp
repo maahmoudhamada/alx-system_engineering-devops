32817f74926a465ea490bf32bc8e9fd61cc6d985
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-06-28 23:50:04 +0300

    0x0A -> 1-install_a_package.pp "Adding Wekzeug pacakge"

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,15 @@
 # Script to install flask pacakge with pip3 using puppet
 
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  name     => 'Werkzeug',
+  provider => 'pip3',
+}
+
 package { 'flask':
 ensure   => '2.1.0',
 name     => 'flask',
 provider => 'pip3',
+require  => Package['Werkzeug'],
 }
+
